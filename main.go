a8175227d5ccaaf96b6fb1ad34718353e64f7390
acaldelas <ajcaldelas@gmail.com>
2025-04-02 21:54:44 -0500

    Added a task struct and method to create tasks

--- main.go
+++ main.go
@@ -6,11 +6,52 @@ import (
   "fmt"
   "strings"
 )
+
+// Define a new Task structure
+type Task struct{
+  Name string
+  Description string
+  Status string 
+}
+
 // Print Option writes out a separation 
 func print_default(s string) {
   fmt.Println("==========================")
   fmt.Println(s)
   fmt.Println("==========================")
+}
+
+func create_task() []Task {
+  // Init tasks
+  var tasks []Task
+  var q string
+
+  // Loop until user quits
+  for q != "quit" {
+    var newTask Task
+    scanner := bufio.NewReader(os.Stdin)
+    fmt.Println("Create a new task")
+    newTask.Name,_ = scanner.ReadString('\n')
+    fmt.Println("Enter a description")
+    newTask.Description,_ = scanner.ReadString('\n')
+    fmt.Println("Enter a status")
+    newTask.Status,_ = scanner.ReadString('\n')
+    
+    //Add to the list of tasks
+    tasks = append(tasks, newTask)
+
+    //Ask user if they want to continue
+    fmt.Println("Successfully created task! New task? y/n")
+    q, _ = scanner.ReadString('\n') 
+    q = strings.TrimSpace(q)
+
+    if q == "y" || q == "Y" {
+      q = ""
+    } else {
+      return tasks
+    }
+  }
+  return tasks
 }
 
 // User input retrieves a user input for the main menu
@@ -26,10 +67,13 @@ func user_input() string{
   option, _ := scanner.ReadString('\n')
   option = strings.TrimSpace(option)
 
+  var tasks []Task
+
   // Set option based on user input
   switch option {
   case "a":
     option = "Add a new task"
+    tasks = create_task()
   case "d":
     option = "Delete a task"
   case "e":
@@ -39,7 +83,12 @@ func user_input() string{
     option = "NoOp"
   }
 
-
+  if tasks != nil {
+    fmt.Println("Here is teh current list of tasks")
+    for _,t := range tasks {
+      print_default(t.Name)
+    }
+  }
   return option
 }
 
